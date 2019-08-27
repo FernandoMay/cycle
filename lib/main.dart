504835228ef41fddf593cf5524007587f05a8b12
Fernando May <fmay130@gmail.com>
2019-08-27 16:27:49 -0500

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'model.dart';
+import 'style.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bicycle',
       theme: ThemeData(primarySwatch: Colors.purple),
       home: MyHomePage(),
@@ -16,51 +18,112 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  var producto = products[0];
+  final producto = products[0];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 500,
-            width: double.infinity,
-            child: LayoutBuilder(
-              builder: (cx, ct) {
-                double w = ct.maxWidth;
-                double h = ct.maxHeight;
-                return Stack(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: w * .9,
-                        height: h * .75,
-                        color: Colors.purpleAccent,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 500,
+              width: double.infinity,
+              child: LayoutBuilder(
+                builder: (cx, ct) {
+                  double w = ct.maxWidth;
+                  double h = ct.maxHeight;
+                  return Stack(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: w * .9,
+                          height: h * .75,
+                          color: Colors.deepPurpleAccent.withOpacity(.40),
+                        ),
                       ),
-                    ),
-                    Positioned(
-                      top: 50.0,
-                      left: 65.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(producto.type),
-                          SizedBox(
-                            height: 12,
+                      Positioned(
+                        top: 50.0,
+                        left: 65.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              producto.type,
+                              style: AppStyle.appbarStyle,
+                            ),
+                            SizedBox(
+                              height: 12.0,
+                            ),
+                            Text(
+                              producto.name,
+                              style: AppStyle.headingStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        child: Hero(
+                          tag: producto.name,
+                          child: Image.asset(
+                            "bike.png",
+                            width: 370,
+                            height: 350,
                           ),
-                        ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    producto.description,
+                    style: AppStyle.headingStyle2,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    producto.power,
+                    style: AppStyle.headingStyle2,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    producto.descPost,
+                    style: AppStyle.descStyle,
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 22.0),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        onPressed: () {},
                       ),
                     ),
-                  ],
-                );
-              },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/model.dart
+++ lib/model.dart
@@ -3,15 +3,28 @@ class Product {
   String type;
   String imgUrl;
   String price;
+  String description;
+  String power;
+  String descPost;
 
-  Product({this.imgUrl, this.name, this.price, this.type});
+  Product(
+      {this.imgUrl,
+      this.name,
+      this.price,
+      this.power,
+      this.type,
+      this.description,
+      this.descPost});
 }
 
 var products = [
   Product(
-    imgUrl: "images/bike.png",
+    imgUrl: "bike.png",
     name: "6MW K30",
     price: "999",
     type: "Bicycle",
+    description: "Amazing Speed.",
+    power: "Incredible Power.",
+    descPost: "The Top Fuel 9.9 XX1 AXS is built for Speed and Capability on Any Terrain!",
   ),
 ];
